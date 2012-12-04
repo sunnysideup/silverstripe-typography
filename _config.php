b98b147ab6f775085b0bf47368c55488e8eebf99
andrew <andrew@sunnysideup.co.nz>
2012-12-04 04:12:47 +0000

    Module made SS 3 compatible
    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/typography/trunk@4285 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- _config.php
+++ _config.php
@@ -7,7 +7,5 @@
 
 //copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
 //===================---------------- START typography MODULE ----------------===================
-//TypographyTestPage::set_auto_include(true);
+//TypographyTestPage::$auto_include = true;
 //===================---------------- END typography MODULE ----------------===================
-
-
