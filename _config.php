9e7a6c08667dee5b516c6f1295685a60c4b0b455
Nicolaas <modules@sunnysideup.co.nz>
2012-12-24 03:39:17 +0000

    ENHANCEMENT: small fix
    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/typography/trunk@4333 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- _config.php
+++ _config.php
@@ -7,5 +7,7 @@
 
 //copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
 //===================---------------- START typography MODULE ----------------===================
-//TypographyTestPage::$auto_include = true;
+//MAY SET
+//TypographyTestPage::set_auto_include(true);
+//TypographyTestPage::set_parent_url_segment('development-only-pages');
 //===================---------------- END typography MODULE ----------------===================
