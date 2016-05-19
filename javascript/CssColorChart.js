72a8c3333c39677ae3866c0eeea6350684d058f3
Nicolaas <github@sunnysideup.co.nz>
2016-05-19 13:38:36 +1200

    better typography

--- javascript/CssColorChart.js
+++ javascript/CssColorChart.js
@@ -1,5 +1,4 @@
-function ViewFiles(id) {
-	var e = document.getElementById('matches_'+id);
-	if (e.style.display == 'block') e.style.display = 'none';
-	else e.style.display = 'block';
-}
+/**
+ * no longer required - see typography.js
+ *
+ */
