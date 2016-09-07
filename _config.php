d045ebb19e23953db316a142e9be6b6c34063e17
Nicolaas <github@sunnysideup.co.nz>
2016-09-08 09:33:28 +1200

    fixes

--- _config.php
+++ _config.php
@@ -5,3 +5,20 @@
  * author: Nicolaas - modules [at] sunnysideup.co.nz
 **/
 
+
+HtmlEditorConfig::get('cms')->setOption(
+    'valid_styles',
+    array('*' => 'color,font-weight,font-style,text-decoration')
+);
+HtmlEditorConfig::get('cms')->setOption(
+    'paste_as_text',
+    true
+);
+HtmlEditorConfig::get('cms')->setOption(
+    'paste_text_sticky',
+    true
+);
+HtmlEditorConfig::get('cms')->setOption(
+    'paste_text_sticky_default',
+    true
+);
