04c8606bae416e1ed4a8e7ca0e635f879df4b185
Sunny Side Up <github@sunnysideup.co.nz>
2017-08-29 15:14:03 +1200

    MINOR: change links automagically

--- _config.php
+++ _config.php
@@ -7,19 +7,23 @@
 
 //HtmlEditorConfig::get('cms')->setOption('ContentCSS', 'themes/main/css/typography.css');
 
-HtmlEditorConfig::get('cms')->setOption(
+HtmlEditorConfig::get('cms')
+->setOption(
     'valid_styles',
     array('*' => 'color,font-weight,font-style,text-decoration')
-);
-HtmlEditorConfig::get('cms')->setOption(
+)
+->setOption(
     'paste_as_text',
     true
-);
-HtmlEditorConfig::get('cms')->setOption(
+)
+->setOption(
     'paste_text_sticky',
     true
-);
-HtmlEditorConfig::get('cms')->setOption(
+)
+->setOption(
     'paste_text_sticky_default',
     true
+)
+->enablePlugins(
+    'autolink'
 );
