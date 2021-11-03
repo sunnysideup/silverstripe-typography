4dad068e8d86e8f25fb215efefe9510374592ac0
nicolaas @ tappy <github@sunnysideup.co.nz>
2021-11-03 18:49:51 +1300

    WIP

--- _config.php
+++ _config.php
@@ -1,14 +1,1 @@
 <?php
-
-/**
- * developed by www.sunnysideup.co.nz
- * author: Nicolaas - modules [at] sunnysideup.co.nz.
- */
-
-use Sunnysideup\Typography\Api\HtmlEditorConfigExtras;
-
-//HtmlEditorConfig::get('cms')->setOption('ContentCSS', 'themes/main/css/typography.css');
-
-if (0 === strpos(ltrim($_SERVER['REQUEST_URI'], '/'), 'admin')) {
-    HtmlEditorConfigExtras::init();
-}
